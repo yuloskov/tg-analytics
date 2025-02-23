003921ae837f547f927cbc8d7995d5d9a887127e
ayuloskov <yuloskov.aa@yandex.ru>
2025-02-23 09:42:25 +0300

    Add analytics

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import { type ReactNode } from "react";
 import { Header } from "./Header";
+import Script from "next/script";
 
 interface LayoutProps {
   children: ReactNode;
@@ -10,8 +11,22 @@ interface LayoutProps {
 export function Layout({ children, activeTab, onTabChange }: LayoutProps) {
   return (
     <div className="container mx-auto p-4">
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-HJZDZTJBCK"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){window.dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-HJZDZTJBCK');
+          `}
+      </Script>
+
       <Header activeTab={activeTab} onTabChange={onTabChange} />
       {children}
     </div>
   );
-} 
+}
